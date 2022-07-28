ef4abf64061c571c9435df8cb42caf9d05c8cc74
nickpan2008 <79726208@qq.com>
2022-07-29 02:08:38 +0800

    ok

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,15 +2,31 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  *
  * @author Wang926454
  * @date 2018/11/16 19:29
  */
+//@SpringBootApplication
+//@tk.mybatis.spring.annotation.MapperScan("com.example.mapper")
+//public class Application {
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
+
 @SpringBootApplication
 @tk.mybatis.spring.annotation.MapperScan("com.example.mapper")
-public class Application {
+public class Application  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
